f0d343bd233039ca2ac0a758fcdbb8b115d8459e
pupilcc <cc@pupilcc.com>
2023-09-23 19:23:33 +0800

    feat: 发送消息的 POST 请求使用 JSON

--- src/main/java/com/pupilcc/pushbot/controller/MessageController.java
+++ src/main/java/com/pupilcc/pushbot/controller/MessageController.java
@@ -4,9 +4,7 @@ import com.pupilcc.common.rest.ApiResult;
 import com.pupilcc.pushbot.entity.SendMessageDTO;
 import com.pupilcc.pushbot.entity.TemplateMessageDTO;
 import com.pupilcc.pushbot.service.MessageService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 消息控制器
@@ -28,8 +26,20 @@ public class MessageController {
      * @param chatToken 用户token
      * @return 响应消息
      */
-    @RequestMapping("/sendMessage/{chatToken}")
+    @GetMapping("/sendMessage/{chatToken}")
     public ApiResult<Object> sendMessage(SendMessageDTO dto, @PathVariable String chatToken) {
+        return messageService.sendMessage(dto, chatToken);
+    }
+
+    /**
+     * 给用户发送消息
+     *
+     * @param dto       消息体
+     * @param chatToken 用户token
+     * @return 响应消息
+     */
+    @PostMapping("/sendMessage/{chatToken}")
+    public ApiResult<Object> sendMessageByJson(@RequestBody SendMessageDTO dto, @PathVariable String chatToken) {
         return messageService.sendMessage(dto, chatToken);
     }
 
@@ -40,8 +50,20 @@ public class MessageController {
      * @param chatToken 用户token
      * @return 响应消息
      */
-    @RequestMapping("/sendTemplate/{chatToken}")
+    @GetMapping("/sendTemplate/{chatToken}")
     public ApiResult<Object> sendTemplateMessage(TemplateMessageDTO message, @PathVariable String chatToken) {
+        return messageService.sendTemplate(message, chatToken);
+    }
+
+    /**
+     * 给用户发送模板消息
+     *
+     * @param message       请求参数
+     * @param chatToken 用户token
+     * @return 响应消息
+     */
+    @PostMapping("/sendTemplate/{chatToken}")
+    public ApiResult<Object> sendTemplateMessageByJson(@RequestBody TemplateMessageDTO message, @PathVariable String chatToken) {
         return messageService.sendTemplate(message, chatToken);
     }
 }
